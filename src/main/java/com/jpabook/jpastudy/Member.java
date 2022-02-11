18a13c721fcdc294830d2ca4bf557556cfa1e48d
rewq985 <bnbt3@naver.com>
2022-02-11 23:54:43 +0900

    <study> 몰라몰라

--- src/main/java/com/jpabook/jpastudy/Member.java
+++ src/main/java/com/jpabook/jpastudy/Member.java
@@ -3,6 +3,7 @@ package com.jpabook.jpastudy;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -15,5 +16,7 @@ public class Member {
     @Id
     @GeneratedValue
     private Long id;
+    @Column(name = "username")
     private String username;
+
 }
