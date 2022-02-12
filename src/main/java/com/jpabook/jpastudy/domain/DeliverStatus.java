2e32874ecaef127772d0538c87c33d8ebf8e58c7
rewq985 <bnbt3@naver.com>
2022-02-12 15:07:12 +0900

    <study> 엔티티 클래스 개발
    - 도메인 모델과 테이블 설계
    - @ManyToOne, @OneToMany 상관관계 공부

---
+++ src/main/java/com/jpabook/jpastudy/domain/DeliverStatus.java
@@ -0,0 +1,5 @@
+package com.jpabook.jpastudy.domain;
+
+public enum DeliverStatus {
+    READY, COMP
+}
---
+++ src/main/java/com/jpabook/jpastudy/domain/Delivery.java
@@ -0,0 +1,27 @@
+package com.jpabook.jpastudy.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliverStatus status; //READY, COMP
+
+}
