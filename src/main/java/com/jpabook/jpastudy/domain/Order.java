230aa4788e92779584237f4252a5463a20a1bd93
rewq985 <bnbt3@naver.com>
2022-02-13 15:33:53 +0900

    <study> 엔티티 클래스 개발 주의사항
    - 모든 연관관계는 지연로딩으로 설정
      즉시 로딩은 예측이 어렵고, 어떤 sql이 샐행될지 추적하기 어렵다. 특히 JPQL을 실행할 떄 N+1 문제가 자주 발생한다.
    - 연관된 엔티티를 함께DB에서 조회해야 하면, fetch join 또는 엔티티 그래프 기능을 사용한다.
    
    - @XToOne 관계는 기본이 즉시로딩이므로 지연로딩으로 바꿔주어야 한다.
    
    - 컬렉션은 반드시 필드에서 초기화 하자
    
    - 셋터는 설정하지 말자

--- src/main/java/com/jpabook/jpastudy/domain/Order.java
+++ src/main/java/com/jpabook/jpastudy/domain/Order.java
@@ -9,6 +9,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")
 @Getter
@@ -19,20 +21,44 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @JsonIgnore
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     @JsonIgnore
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
-    private LocalDateTime orderDate;
+    private LocalDateTime orderDate;//주문시간
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문 상태 [ORDER, CANCEL]
+
+    // == 연관관계 메서드 == //
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+    public static void main(String[] args) {
+        Member member = new Member();
+        Order order = new Order();
+
+        order.setMember(member);
+    }
 }
