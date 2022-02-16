03c0e00ead1d39b5177b99161afb79204906557f
han <dn@codeclick.co.kr>
2022-02-16 13:44:10 +0900

    <study> jpa 공부
    - 상품 비즈니스 로직 추가

--- src/main/java/com/jpabook/jpastudy/domain/item/Item.java
+++ src/main/java/com/jpabook/jpastudy/domain/item/Item.java
@@ -35,7 +35,7 @@ public abstract class Item {
     /**
      * stock 증가
      */
-    public void addStockEntity(int quantity) {
+    public void addStock(int quantity) {
         this.stockQuantity += quantity;
     }
 
