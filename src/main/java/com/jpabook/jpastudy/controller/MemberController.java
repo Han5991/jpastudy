6f0e54ec88ede16ffb06408a0d8a4ab426a656a0
rewq985 <bnbt3@naver.com>
2022-02-20 09:34:40 +0900

    <study> 홈화면/회원가입화면 추가
    - 바인딩 다시 확인

---
+++ src/main/java/com/jpabook/jpastudy/controller/MemberController.java
@@ -0,0 +1,44 @@
+package com.jpabook.jpastudy.controller;
+
+import com.jpabook.jpastudy.domain.Address;
+import com.jpabook.jpastudy.domain.Member;
+import com.jpabook.jpastudy.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("members")
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
